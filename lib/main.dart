b1782e6b7becc4abf1416f63e587d76ff1f587b4
Liew3863e <liew8444@gmail.com>
2022-07-26 01:59:52 +0700

    เปลี่ยนสีแอพ

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("กระบองเพชร By My Home"),
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("กระบองเพชร By My Home"),
+        ),
+        body: Text("เชิญชมสินค้าของเรา"),
       ),
-      body: Text("เชิญชมสินค้าของเรา"),
-    ),
-  );
+      theme: ThemeData(primarySwatch: Colors.green)
+      //เปลี่ยนสี
+      );
   runApp(app);
 }
