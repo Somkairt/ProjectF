586500eadcfc96dfa451f877abb1afa9ad0bb95e
Liew3863e <liew8444@gmail.com>
2022-07-26 02:22:32 +0700

    สร้างwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-      title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("กระบองเพชร By My Home"),
+  runApp(MyApp());
+}
+
+//สร้าง widget
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My App",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("กระบองเพชร By My Home"),
+          ),
+          body: Text("เชิญชมสินค้าของเรา"),
         ),
-        body: Text("เชิญชมสินค้าของเรา"),
-      ),
-      theme: ThemeData(primarySwatch: Colors.green)
-      //เปลี่ยนสี
-      );
-  runApp(app);
+        theme: ThemeData(primarySwatch: Colors.green) //เปลี่ยนสี
+        );
+  }
 }
